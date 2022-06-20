9699e9ad090448907a40d9959d2a9f7beb39b085
FrMks <maxfrancuzov@gmail.com>
2022-06-20 14:49:53 +0300

    First commit

---
+++ LOLQuiz/LOLQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOLQuizApp.swift
+//  LOLQuiz
+//
+//  Created by Максим Французов on 20.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct LOLQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
