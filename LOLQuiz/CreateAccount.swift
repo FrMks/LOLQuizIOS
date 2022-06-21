7acdbf249a7990c7285e820de96896124cd2724f
FrMks <maxfrancuzov@gmail.com>
2022-06-21 12:39:10 +0300

    + email

--- LOLQuiz/CreateAccount.swift
+++ LOLQuiz/CreateAccount.swift
@@ -26,12 +26,27 @@ struct CreateAccount: View {
                     Circle()
                         .opacity(0.7)
                         .foregroundColor(Color(red: 75/255, green: 123/255, blue: 229/255))
-                        .frame(width: 200, height: 200)
+                        .frame(width: 150, height: 150)
                     
                     Image("AccountIcon")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 200, height: 200)
+                        .frame(width: 150, height: 150)
+                }
+                
+                HStack {
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 50, height: 50)
+                            .cornerRadius(10)
+                            .foregroundColor(Color(red: 168/255, green: 92/255, blue: 249/255))
+                        .opacity(0.7)
+                        
+                        Image("Email_SVG")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.white)
+                    }
                 }
             }
         }
